9a8d380142e48649d13c29dd11e1570ce079d1cc
Alexv035 <voronkov.a.g@gmail.com>
2022-01-03 19:00:59 +0300

    Lesson 3. Django ORM start

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -10,13 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-import os
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
@@ -130,3 +129,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
