7a1e9b3032d73845f75fd98d07d111a405974b2f
Alexv035 <voronkov.a.g@gmail.com>
2022-01-09 17:43:31 +0300

    Lesson 4. Data from files

--- mainapp/migrations/0003_fill_db.py
+++ mainapp/migrations/0003_fill_db.py
@@ -6,8 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mainapp', '0002_contact'),
+        ("mainapp", "0002_contact"),
     ]
 
-    operations = [
-    ]
+    operations = []
