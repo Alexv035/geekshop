c2a8eda6c642b967ab3627963445900b748093e1
Alexv035 <voronkov.a.g@gmail.com>
2022-01-15 21:36:10 +0300

    Lesson 5. Basket counter

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,30 +1,42 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
-<<<<<<< HEAD
 from .models import Contact, Product, ProductCategory
-=======
-from .models import Product, ProductCategory
->>>>>>> 3456eceaa0cf341aec26164a3effe00ef62ce97a
 
 
 def main(request):
     title = "главная"
 
-<<<<<<< HEAD
     products = Product.objects.all()[:4]
-=======
-    products = Product.objects.all()
->>>>>>> 3456eceaa0cf341aec26164a3effe00ef62ce97a
 
-    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
+    content = {
+        "title": title,
+        "products": products,
+        "media_url": settings.MEDIA_URL
+    }
     return render(request, "mainapp/index.html", content)
 
 
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by("price")
+            category = {"name": "все"}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(
+                category__pk=pk).order_by("price")
+        content = {
+            "title": title,
+            "links_menu": links_menu,
+            "category": category,
+            "products": products,
+            "media_url": settings.MEDIA_URL,
+        }
+        return render(request, "mainapp/products_list.html", content)
     same_products = Product.objects.all()
     content = {
         "title": title,
@@ -40,24 +52,10 @@ def products(request, pk=None):
 def contact(request):
     title = "о нас"
     visit_date = timezone.now()
-<<<<<<< HEAD
     locations = Contact.objects.all()
-=======
-    locations = [
-        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
-        {
-            "city": "Екатеринбург",
-            "phone": "+7-777-777-7777",
-            "email": "info_yekaterinburg@geekshop.ru",
-            "address": "Близко к центру",
-        },
-        {
-            "city": "Владивосток",
-            "phone": "+7-999-999-9999",
-            "email": "info_vladivostok@geekshop.ru",
-            "address": "Близко к океану",
-        },
-    ]
->>>>>>> 3456eceaa0cf341aec26164a3effe00ef62ce97a
-    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    content = {
+        "title": title,
+        "visit_date": visit_date,
+        "locations": locations
+    }
     return render(request, "mainapp/contact.html", content)
