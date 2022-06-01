c2e6f5c4718deb4f7e02960852c002b49e04a09d
Alexv035 <voronkov.a.g@gmail.com>
2022-06-02 00:41:04 +0300

    Lesson 7. Code style fix

--- adminapp/views.py
+++ adminapp/views.py
@@ -18,10 +18,8 @@ def admin_main(request):
 @user_passes_test(lambda u: u.is_superuser)
 def users(request):
     title = "админка/пользователи"
-    users_list = ShopUser.objects.all().order_by(
-        "-is_active", "-is_superuser", "-is_staff", "username")
-    content = {"title": title, "objects": users_list,
-               "media_url": settings.MEDIA_URL}
+    users_list = ShopUser.objects.all().order_by("-is_active", "-is_superuser", "-is_staff", "username")
+    content = {"title": title, "objects": users_list, "media_url": settings.MEDIA_URL}
     return render(request, "adminapp/users.html", content)
 
 
@@ -36,8 +34,7 @@ def user_create(request):
     else:
         user_form = ShopUserRegisterForm()
 
-    content = {"title": title, "update_form": user_form,
-               "media_url": settings.MEDIA_URL}
+    content = {"title": title, "update_form": user_form, "media_url": settings.MEDIA_URL}
 
     return render(request, "adminapp/user_update.html", content)
 
@@ -47,16 +44,14 @@ def user_update(request, pk):
 
     edit_user = get_object_or_404(ShopUser, pk=pk)
     if request.method == "POST":
-        edit_form = ShopUserAdminEditForm(
-            request.POST, request.FILES, instance=edit_user)
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
         if edit_form.is_valid():
             edit_form.save()
             return HttpResponseRedirect(reverse("admin:user_update", args=[edit_user.pk]))
     else:
         edit_form = ShopUserAdminEditForm(instance=edit_user)
 
-    content = {"title": title, "update_form": edit_form,
-               "media_url": settings.MEDIA_URL}
+    content = {"title": title, "update_form": edit_form, "media_url": settings.MEDIA_URL}
 
     return render(request, "adminapp/user_update.html", content)
 
@@ -73,8 +68,7 @@ def user_delete(request, pk):
         user.save()
         return HttpResponseRedirect(reverse("admin:users"))
 
-    content = {"title": title, "user_to_delete": user,
-               "media_url": settings.MEDIA_URL}
+    content = {"title": title, "user_to_delete": user, "media_url": settings.MEDIA_URL}
 
     return render(request, "adminapp/user_delete.html", content)
 
@@ -83,8 +77,7 @@ def user_delete(request, pk):
 def categories(request):
     title = "админка/категории"
     categories_list = ProductCategory.objects.all()
-    content = {"title": title, "objects": categories_list,
-               "media_url": settings.MEDIA_URL}
+    content = {"title": title, "objects": categories_list, "media_url": settings.MEDIA_URL}
     return render(request, "adminapp/categories.html", content)
 
 
@@ -108,8 +101,7 @@ def products(request, pk):
     title = "админка/продукт"
     category = get_object_or_404(ProductCategory, pk=pk)
     products_list = Product.objects.filter(category__pk=pk).order_by("name")
-    content = {"title": title, "category": category,
-               "objects": products_list, "media_url": settings.MEDIA_URL}
+    content = {"title": title, "category": category, "objects": products_list, "media_url": settings.MEDIA_URL}
     return render(request, "adminapp/products.html", content)
 
 
